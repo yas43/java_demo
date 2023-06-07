86adc56d2b6bc63accceb1e3c31f1cd319baa604
yaser <ykeshtdar@yahoo.com>
2023-06-07 15:35:36 +0200

    first commit

---
+++ this_demo0.java
@@ -0,0 +1,40 @@
+package forth_pro;
+
+class somme {
+	int num1;
+	int num2;
+
+	somme(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+		System.out.println("first constructor running  " + num1 + "  and  " + num2);
+	}
+
+	somme(int num1) {
+		this(num1, num1);
+	}
+
+	somme() {
+		this(0);
+	}
+
+	void my_method() {
+		System.out.println("this is my_method running");
+	}
+}
+
+public class this_demo0 {
+
+	static void demo_method(int a, int b, int c, int d) {
+		System.out.println("thi is a " + a + "  this is b  " + b + "  this is c  " + c + "  this is d  " + d);
+	}
+
+	public static void main(String[] args) {
+		// somme my_set = new somme(10, 15);
+		// somme my_set_0 = new somme(30);
+		// somme my_set_1 = new somme();
+		// my_set.my_method();
+		demo_method(10, 20, 30, 40);
+	}
+
+}
